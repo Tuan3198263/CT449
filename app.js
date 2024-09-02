a7b5425ad274dd645032e393bf1c8bee29779e01
TuanLe <steve29012003@gmail.com>
2024-09-02 15:03:56 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contactsRoutetr = require("./app/route/contact.route");
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contack book aplication." });
 });
+
+app.use("/api/contacts", contactsRoutetr)
 
 module.exports = app;
