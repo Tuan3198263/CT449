fa65d1e8e86d666af589acac94d020147065e301
TuanLe <steve29012003@gmail.com>
2024-09-02 15:30:08 +0700

    Cai dat xu li loi

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const contactsRoutetr = require("./app/route/contact.route");
+const contactsRouter = require("./app/route/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,16 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contack book aplication." });
 });
 
-app.use("/api/contacts", contactsRoutetr)
+app.use("/api/contacts", contactsRouter)
+
+app.use((req, res, next) => {
+    return next( new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) =>{
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
